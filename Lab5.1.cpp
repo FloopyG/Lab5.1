162b0bbc267b4ad80c7c30252c6ac6b857db1ce5
FloopyG <36450035+FloopyG@users.noreply.github.com>
2020-11-01 19:57:07 +0200

    Initial commit

---
+++ Lab5.1.cpp
@@ -0,0 +1,28 @@
+﻿
+
+#include <iostream>
+#include <cmath>;
+
+using namespace std;
+
+double f(const double a, const double b, const double c);
+
+int main()
+{
+    double s, t, P;
+    cout << "s = "; cin >> s;
+    cout << "t = "; cin >> t;
+
+    P = (f(1, t+s, s) + f(t, t * s, 1)) / (1 + f(s, 1, t) * f(s, 1, t));
+
+    cout << P << endl;
+
+    cin.get();
+    system("pause");
+    return 0;
+}
+
+double f(const double a, const double b, const double c) {
+    return a * sin(b) + b * sin(a) * c * c;
+}
+
